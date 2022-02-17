7393fe835c10d6c2f87f80299ad5ad6c41863e4f
whalepopov <popovaa07@gmail.com>
2022-02-17 22:10:57 +0300

    initial commit

---
+++ src/test/java/ru/events/afisha/AfishaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.events.afisha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfishaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
